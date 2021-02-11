4fbdcb51bc2cda186049586abe7b252cc956878a
CapArken <aizhe5757@gmail.com>
2021-02-11 10:03:31 -0500

    Added logo to main page

---
+++ src/components/logo.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-wrapper">
+                <img alt="DailySmarty Logo" src="assets/ds_circle_logo.png" />
+            </div>
+        )
+    }
+}
